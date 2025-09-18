c056598e74c366b4eff9f4b31590d77c9d91487d
Nayim Ali <nayimali@Nayims-Mac-mini.local>
2025-09-18 23:37:40 +0100

    Allow build with TypeScript errors by setting ignoreBuildErrors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* other config options here */
+
+  typescript: {
+    // ✅ build even if there are type errors
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
